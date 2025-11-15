4ae26cc365ced51f8a7b8e0ee5edf0473300d60f
quarkusio <no-reply@quarkus.io>
2025-11-15 00:32:16 +0000

    Initial commit

---
+++ src/test/java/org/sunat/exchange/rate/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.sunat.exchange.rate;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
